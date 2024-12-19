6aff5acf06c6911c5561356410b94b4008a7ac0a
Abdul Moiz Anwer <89318559+AbdulMoizAnwer@users.noreply.github.com>
2024-12-19 21:58:20 +0100

    Update yourName.py

--- yourName.py
+++ yourName.py
@@ -3,3 +3,12 @@ while name != 'your name':
   print('Please type your name.')
   name = input()
 print('Thank you!')
+
+# OR
+
+while True:
+    print('Please type your name.')
+    name = input()
+    if name == 'your name':
+        break
+print('Thank you!')
